8ac9eeaa87814a26c582b0f0bdf2cac39244ec0e
Ashley Grevelink <ashlink1111@gmail.com>
2024-03-29 10:47:30 -0400

    CMakeLists.txt is now runnable w bash and GUI

---
+++ CMakeFiles/ModuleMakerTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModuleMakerTest.dir/src/ModuleMakerTest.cpp.o"
+  "CMakeFiles/ModuleMakerTest.dir/src/ModuleMakerTest.cpp.o.d"
+  "bin/ModuleMakerTest"
+  "bin/ModuleMakerTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModuleMakerTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/ModuleMakerTest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ModuleMakerTest.
+# This may be replaced when dependencies are built.
